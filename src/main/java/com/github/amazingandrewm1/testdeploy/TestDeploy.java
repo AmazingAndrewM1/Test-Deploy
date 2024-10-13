1ca70033fe3684c973fc407e874297ac2d317000
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-10-13 19:11:38 -0400

    Revert to original boilerplate to check functionality.

--- src/main/java/com/github/amazingandrewm1/testdeploy/TestDeploy.java
+++ src/main/java/com/github/amazingandrewm1/testdeploy/TestDeploy.java
@@ -1,19 +1,51 @@
 package com.github.amazingandrewm1.testdeploy;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import java.awt.BorderLayout;
+public class TestDeploy extends JFrame {
 
-public class TestDeploy extends JFrame{
+    private Timer timer;
+    private int x, y, xDelta = 2, yDelta = 2;
+    private JButton startButton, stopButton;
+    private AnimatedPanel animatedPanel;
+
     public TestDeploy() {
+        x = y = 100;
+        timer = new Timer(10, e -> {
+            x += xDelta;
+            y += yDelta;
+
+            if (x + "Hello jDeploy".length() * 7 >= animatedPanel.getWidth() || x < 0) xDelta *= -1;
+            if (y + 15 >= animatedPanel.getHeight() || y < 0) yDelta *= -1;
+
+            animatedPanel.repaint();
+        });
+
+        startButton = new JButton("Start");
+        startButton.addActionListener(e -> timer.start());
+
+        stopButton = new JButton("Stop");
+        stopButton.addActionListener(e -> timer.stop());
+
+        animatedPanel = new AnimatedPanel();
+
         setLayout(new BorderLayout());
-        add(new AnimatedPanel(), BorderLayout.CENTER);
+        add(animatedPanel, BorderLayout.CENTER);
+        add(startButton, BorderLayout.NORTH);
+        add(stopButton, BorderLayout.SOUTH);
+    }
+
+    class AnimatedPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawString("Hello jDeploy 8", x, y);
+        }
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             TestDeploy frame = new TestDeploy();
-            frame.setTitle("SimpleMouse");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(400, 400);
             frame.setVisible(true);
