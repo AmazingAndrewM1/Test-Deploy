98208b66513a82ca448fa36ec90fd5be9ba69f02
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-10-13 18:50:34 -0400

    Testing to see if extending JFrame class changes to desirable behavior.

--- src/main/java/com/github/amazingandrewm1/testdeploy/TestDeploy.java
+++ src/main/java/com/github/amazingandrewm1/testdeploy/TestDeploy.java
@@ -4,18 +4,17 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 
-public class TestDeploy{
-    private static JFrame frame;
-
-    public TestDeploy() {}
+public class TestDeploy extends JFrame{
+    public TestDeploy() {
+        add(new AnimatedPanel(), BorderLayout.CENTER);
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            frame = new JFrame("SimpleMouse");
+            TestDeploy frame = new TestDeploy();
+            frame.setTitle("SimpleMouse");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(400, 400);
-            frame.setLayout(new BorderLayout());
-            frame.add(new AnimatedPanel(), BorderLayout.CENTER);
             frame.setVisible(true);
         });
     }
