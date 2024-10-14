8586f80dea720e4f2ab9db16091f791a3f2e99ba
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-10-14 09:49:36 -0400

    Commit to test addActionListener() with JButton

--- src/main/java/com/github/amazingandrewm1/testdeploy/TestDeploy.java
+++ src/main/java/com/github/amazingandrewm1/testdeploy/TestDeploy.java
@@ -1,44 +1,75 @@
 package com.github.amazingandrewm1.testdeploy;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
 
-public class TestDeploy extends JFrame {
-
-    private int mouseX = 0;
-    private int mouseY = 0;
-    private final AnimatedPanel animatedPanel;
-
+public class TestDeploy extends JFrame{
     public TestDeploy() {
-        animatedPanel = new AnimatedPanel();
-        animatedPanel.addMouseListener(new MouseListener(){
-            @Override
-            public void mouseClicked(MouseEvent me){
-                mouseX = me.getX();
-                mouseY = me.getY();
-                animatedPanel.repaint();
-            }
-
-            @Override public void mousePressed(MouseEvent me){}
-            @Override public void mouseReleased(MouseEvent me){}
-            @Override public void mouseEntered(MouseEvent me){}
-            @Override public void mouseExited(MouseEvent me){}
-        });
-
         setLayout(new BorderLayout());
-        add(animatedPanel, BorderLayout.CENTER);
-        add(new JButton("Start"), BorderLayout.NORTH);
-        add(new JButton("Stop"), BorderLayout.SOUTH);
+        add(new Board(), BorderLayout.CENTER);
     }
 
-    class AnimatedPanel extends JPanel {
+    private static final class Board extends JComponent {
+        private final int NUM_ROWS = 8;
+        private final int NUM_COLUMNS = 8;
+        private final int SQUARE_SIZE = 50;
+        private int rNum = 0;
+        private int cNum = 0;
+
+        public Board(){
+            this.setPreferredSize(new Dimension(SQUARE_SIZE * NUM_COLUMNS, SQUARE_SIZE * NUM_ROWS));
+
+            JLabel testLabel = new JLabel("Hi");
+            testLabel.setLocation(120, 210);
+            testLabel.setVisible(true);
+
+            for (int r = 0; r < NUM_ROWS; ++r){
+                for (int c = 0; c < NUM_COLUMNS; ++c){
+                    JButton button = new JButton();
+                    button.setContentAreaFilled(false);
+                    button.setFocusPainted(false);
+                    button.setBorderPainted(false);
+                    button.setSize(SQUARE_SIZE, SQUARE_SIZE);
+                    button.setLocation(SQUARE_SIZE * c, SQUARE_SIZE * r);
+
+                    final int R_FINAL = r;
+                    final int C_FINAL = c;
+                    button.addActionListener((ActionEvent e) -> {
+                        rNum = R_FINAL;
+                        cNum = C_FINAL;
+                        repaint();
+                    });
+                    this.add(button);
+                }
+            }
+        }
+
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
-            String text = "Mouse Coordinates: (" + mouseX + ", " + mouseY + ")";
-            g.drawString(text, 100, 100);
+            Graphics2D g2D = (Graphics2D) g;
+            boolean isLight = true;
+            for (int r = 0; r < NUM_ROWS; ++r){
+                for (int c = 0; c < NUM_COLUMNS; ++c){
+                    if (isLight){
+                        g2D.setColor(Color.WHITE);
+                        isLight = false;
+                    }
+                    else{
+                        g2D.setColor(Color.BLACK);
+                        isLight = true;
+                    }
+                    g2D.fillRect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+                }
+                isLight = !isLight;
+            }
+
+            Font currentFont = g2D.getFont();
+            g2D.setFont(currentFont.deriveFont(currentFont.getSize() * 1.5F));
+            g2D.setColor(Color.RED);
+            String text = "(" + rNum + ", " + cNum + ")";
+            g2D.drawString(text, 100, 125);
         }
     }
 
@@ -46,7 +77,7 @@ public class TestDeploy extends JFrame {
         SwingUtilities.invokeLater(() -> {
             TestDeploy frame = new TestDeploy();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(400, 400);
+            frame.pack();
             frame.setVisible(true);
         });
     }
